35255ed699236dd271b97af1151ec89f103d0083
Khory Kotowski <khorykotowski@Khorys-MBP.fios-router.home>
2019-10-14 15:17:41 -0400

    Added example cards, navigation, and acct create button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private alertCtrl: AlertController) {}
+
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+    message: 'Create a new client profile',
+    inputs: [
+      {
+        name: 'name',
+        placeholder: 'Client Name'
+      },
+      {
+        name: 'location',
+        placeholder: 'Password',
+      },
+      {
+      	name: 'date',
+      	placeholder: 'date',
+      	type: 'date'
+      },
+      {
+      	name: "info",
+      	placeholder: "additional client info",
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Create',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      }
+    ]
+   });
+   		await alert.present(); 
+	}
 
 }
