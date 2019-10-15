4b1e35376e813cb8e09850088ffdbc0434dde820
Khory Kotowski <khorykotowski@Khorys-MBP.fios-router.home>
2019-10-15 11:36:13 -0400

    Cleaned up homepage formatting

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,84 +11,76 @@ import { Storage } from '@ionic/storage';
 })
 export class HomePage {
 
-accounts: any[] = [];
+    accounts: any[] = [];
 
- account = {
-   name: "",
-   location: "",
-   date: "",
-   info: "",
-   entitledDeployed: [],
-   cloudInfo: [],
-   questions: [],
-   report: []
- };
+    account = {
+        name: "",
+        location: "",
+        date: "",
+        info: "",
+        entitledDeployed: [],
+        cloudInfo: [],
+        questions: [],
+        report: [] };
 
-  constructor(private alertCtrl: AlertController, private storage: Storage) {
-    storage.get('accounts').then((val)=> {
-      if (val != null) {
-        this.accounts = val;
-      }
-    });
-  }
+    constructor(private alertCtrl: AlertController, private storage: Storage) {
+        storage.get('accounts').then((val)=> {
+            if (val != null) {
+                this.accounts = val;
+            }
+        });
+    }
 
-  async presentAlert() {
-    const alert = await this.alertCtrl.create({
-    message: 'Create a new client profile',
-    inputs: [
-      {
-        name: 'name',
-        placeholder: 'Client Name'
-      },
-      {
-        name: 'location',
-        placeholder: 'location',
-      },
-      {
-      	name: 'date',
-      	placeholder: 'date',
-      	type: 'date'
-      },
-      {
-      	name: "info",
-      	placeholder: "additional client info",
-      }
-    ],
-    buttons: [
-      {
-        text: 'Cancel',
-        role: 'cancel',
-        handler: data => {
-          console.log('Cancel clicked');
-        }
-      },
-      {
-        text: 'Create',
-        handler: data => {
-          console.log('Create clicked');
-          console.log(data);
-          console.log("THIS IS DATA TETS");
-          console.log(data.name);
-                    console.log("THIS IS DATA TETS");
-
-          let newAcct = {
-           name: data.name,
-           location: data.location,
-           date: data.date,
-           info: data.info,
-           entitledDeployed: [],
-           cloudInfo: [],
-           questions: [],
-           report: []
-           };
-           this.accounts.push(newAcct);
-           this.storage.set("accounts", this.accounts);
-
-        }
-      }
-    ]
-   });
-   		await alert.present(); 
-	}
-
+    async presentAlert() {
+        const alert = await this.alertCtrl.create({
+            message: 'Create a new client profile',
+            inputs: [
+                {
+                    name: 'name',
+                    placeholder: 'Client Name'
+                },
+                {
+                    name: 'location',
+                    placeholder: 'location',
+                },
+                {
+                  	name: 'date',
+                  	placeholder: 'date',
+                  	type: 'date'
+                },
+                {
+                  	name: "info",
+                  	placeholder: "additional client info",
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: data => {
+                      console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Create',
+                    handler: data => {
+                        console.log('Create clicked');
+                        let newAcct = {
+                            name: data.name,
+                            location: data.location,
+                            date: data.date,
+                            info: data.info,
+                            entitledDeployed: [],
+                            cloudInfo: [],
+                            questions: [],
+                            report: []
+                        };
+                        this.accounts.push(newAcct);
+                        this.storage.set("accounts", this.accounts);
+                    }
+                }
+            ]
+        });
+        await alert.present(); 
+    }
 }
