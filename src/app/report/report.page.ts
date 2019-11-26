678e28df0d4bb23cf406e8acd8abcd4033409a59
Khory Kotowski <khorykotowski@Khorys-MBP.fios-router.home>
2019-11-26 17:32:36 -0500

    Updated content

--- src/app/report/report.page.ts
+++ src/app/report/report.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
+
 
 @Component({
   selector: 'app-report',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportPage implements OnInit {
 
-  constructor() { }
+account: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private auth: AuthenticationService) { 
+  let that = this;
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.account = this.router.getCurrentNavigation().extras.state.acct;
+          that.auth.database.get(that.auth.userInfo.email).then(function(doc) {
+            if(doc["accounts"] != null)
+            {
+              for(var i = 0; i < doc["accounts"].length; i++)
+              {
+                if (doc["accounts"][i].name === that.account.name)
+                {
+                  doc["accounts"][i] = that.account;
+                }
+              }
+              that.auth.database.put(doc);
+            }
+          });
+      }
+    });
+}
 
   ngOnInit() {
+  }
+
+
+  navigateToPage(page) {
+    let navigationExtras: NavigationExtras = {
+            state: {
+                acct: this.account
+            }
+        };
+        this.router.navigate([page], navigationExtras);
   }
 
 }
