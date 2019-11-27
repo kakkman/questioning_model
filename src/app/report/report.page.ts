06a2575e6869578714eb8a54052222babb131dd2
Khory Kotowski <khorykotowski@Khorys-MBP.fios-router.home>
2019-11-27 18:17:18 -0500

    Cleaned up code

--- src/app/report/report.page.ts
+++ src/app/report/report.page.ts
@@ -10,59 +10,51 @@ import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 })
 export class ReportPage implements OnInit {
 
-account: any;
-questions: any;
-positiveList: any;
+  account: any;
+  questions: any;
+  positiveList: any;
 
   constructor(private route: ActivatedRoute, private router: Router, private auth: AuthenticationService) { 
-  let that = this;
-    this.route.queryParams.subscribe(params => {
-      if (this.router.getCurrentNavigation().extras.state) {
-        this.account = this.router.getCurrentNavigation().extras.state.acct;
-          that.auth.database.get(that.auth.userInfo.email).then(function(doc) {
-            if(doc["accounts"] != null)
-            {
-              for(var i = 0; i < doc["accounts"].length; i++)
+    let that = this;
+      this.route.queryParams.subscribe(params => {
+        if (this.router.getCurrentNavigation().extras.state) {
+          this.account = this.router.getCurrentNavigation().extras.state.acct;
+            that.auth.database.get(that.auth.userInfo.email).then(function(doc) {
+              if(doc["accounts"] != null)
               {
-                if (doc["accounts"][i].name === that.account.name)
+                for(var i = 0; i < doc["accounts"].length; i++)
                 {
-                  doc["accounts"][i] = that.account;
+                  if (doc["accounts"][i].name === that.account.name)
+                  {
+                    doc["accounts"][i] = that.account;
+                  }
                 }
+                that.auth.database.put(doc);
               }
-              that.auth.database.put(doc);
-            }
-          });
-      }
-    });
+            });
+        }
+      });
 
-    var yes = "Because you answered yes to the following questions:";
-    var no = "Because you said no to the following questions:";
     this.auth.prospectingDB.allDocs({include_docs: true}).then(res => {
       this.questions = res.rows;
     });
+  }
 
-
-}
-
-filter(element) {
+  filter(element) {
     var allPaths = [];
     allPaths = element.doc.questions.filter(value => {
-    let obj = {
-      product: element.doc.name,
-      question: value.question,
-      answer: value.good
-    }
-    return this.account.questions.some(item => JSON.stringify(item) == JSON.stringify(obj));
-
-  })
-        console.log(allPaths)
-
+      let obj = {
+        product: element.doc.name,
+        question: value.question,
+        answer: value.good
+      }
+      return this.account.questions.some(item => JSON.stringify(item) == JSON.stringify(obj));
+    });
     return allPaths;
   }
 
   ngOnInit() {
   }
-
 
   navigateToPage(page) {
     let navigationExtras: NavigationExtras = {
@@ -72,5 +64,4 @@ filter(element) {
         };
         this.router.navigate([page], navigationExtras);
   }
-
 }
